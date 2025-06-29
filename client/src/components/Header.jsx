2278c6c35340e0b2eb47120ebc67af5c7be6a4ca
Syed Aasim Ali <syedaasimali334@gmail.com>
2025-06-29 13:23:48 +0530

    Fixing ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
   const dispatch = useDispatch()
   const mode = useSelector(state => state.game.mode)
   return (
-    <div className='flex w-full justify-around items-center h-[10%] text-white font-[cursive] border-b-[0.2px] border-white mb-8'>
-        <div className='font-bold text-xl sm:text-[2rem]'>
+    <div className='flex w-full justify-around items-center h-[10%] text-white font-[cursive] border-b-1 border-white mb-8'>
+        <div className='font-bold text-[1.5rem] sm:text-[2rem]'>
             <span>Chess</span>
         </div>
-        <div className='flex gap-6 sm:gap-12 h-full items-center text-[.6rem] sm:text-xl'>
+        <div className='flex gap-6 sm:gap-12 h-full items-center text-[.8rem] sm:text-xl'>
             <Link
               to="/" 
               className={`${mode === "online" && "border-b-1"} border-white h-[28px] sm:h-[2rem] flex items-end`}
